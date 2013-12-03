80ae487c65abeebdc715a1fbb07221036b832099
bmccann <bmccann@stanford.edu>
2013-12-02 20:19:32 -0800

    Adding feature extractor and pickle files

--- solver/feature_extractor.py
+++ solver/feature_extractor.py
@@ -1,5 +1,7 @@
-from music21 import corpus, roman, key, interval
+import music21
 from collections import Counter
+import pickle
+import re
 
 class ScoreFeatureExtractor:
 	"""
@@ -11,40 +13,67 @@ class ScoreFeatureExtractor:
 			self.streams = [x.parse() for x in bundle]
 		else:
 			self.streams = streams
-		self.chordProgressions = []
-		self.intervalSets = []
-		self.chordFeatures = Counter()
-		self.intervalFeatures = Counter()
 
 	def parse(self):
+		print "Parsing..."
+		self.chordProgressions = []
+		self.intervalSets = []
+
 		for stream in self.streams:
-			reduction = stream.chordify()
+			print '.'
+			try:
+				reduction = stream.chordify()
+			except music21.stream.StreamException:
+				continue
 			analyzedKey = stream.analyze('key')
 			chords = []
 			intervals = []
 			for c in reduction.flat.getElementsByClass('Chord'):
 				c.closedPosition(forceOctave=4, inPlace = True)
-				mainInterval = interval.notesToChromatic(c.pitches[0], c.pitches[-1])
-				rn = roman.romanNumeralFromChord(c, analyzedKey)
+				mainInterval = music21.interval.notesToChromatic(c.pitches[0], c.pitches[-1])
+				rn = music21.roman.romanNumeralFromChord(c, analyzedKey)
 				chords.append((rn.scaleDegree, rn.quality, rn.inversion()))
 				intervals.append(mainInterval)
 			self.chordProgressions.append(chords)
 			self.intervalSets.append(intervals)
+			pickle.dump(self.chordProgressions, open('chords', 'w'))
+			pickle.dump(self.intervalSets, open('intervals', 'w'))
+
 
 	def extractNgrams(self, sizes):
+		print "Extracting features..."
+		self.chordFeatures = Counter()
+		self.intervalFeatures = Counter()
+
 		sizes = [x-1 for x in sizes]
 		for chordSet in self.chordProgressions:
 			for idx, chord in enumerate(chordSet):
-				key = (chord,)
+				key = [chord]
 				for n in sizes:
-					if (idx < n or (idx + n) == len(chordSet)): continue
-					key += (chordSet[idx+n],)
-					self.chordFeatures[key] += 1
+					# print '..'
+					if (idx >= n): 
+						key += chordSet[idx-n:idx]
+						self.chordFeatures[tuple(key)] += 1
 
 		for intervalSet in self.intervalSets:
 			for idx, interval in enumerate(intervalSet):
-				key = (interval,)
+				key = [interval]
+				# print re.search('([0-9]+)>', str(interval)).group(0)[:-1]
 				for n in sizes:
-					if (idx < n or (idx + n) == len(intervalSet)): continue
-					key += (intervalSet[idx+n],)
-					self.intervalFeatures[key] += 1
+					# print '...'
+					if (idx >= n): 
+						key += intervalSet[idx-n:idx]
+						self.intervalFeatures[tuple(key)] += 1
+		print "...features extracted"
+
+
+if __name__ == "__main__":
+	sfe = ScoreFeatureExtractor(bundle = music21.corpus.search('bach'))
+	# sfe.parse() 
+	sfe.chordProgressions = pickle.load(open('chords', 'r'))
+	sfe.intervalSets = pickle.load(open('intervals', 'r'))
+	# sfe.extractNgrams([2,3,4])
+	# pickle.dump(sfe.chordFeatures, open('chordFeatures', 'w'))
+	# pickle.dump(sfe.intervalFeatures, open('intervalFeatures', 'w'))
+	sfe.chordFeatures = pickle.load(open('chordFeatures', 'r'))
+	sfe.intervalFeatures = pickle.load(open('intervalFeatures', 'r'))
