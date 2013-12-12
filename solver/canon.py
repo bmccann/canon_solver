c07c3a1fdfbf3a6c02ce79e28cc0d63b1e7ccc25
bmccann <bmccann@stanford.edu>
2013-12-11 22:19:33 -0800

    before cleaning up code

--- solver/canon.py
+++ solver/canon.py
@@ -1,19 +1,21 @@
 import util, math, music21
-from featureExtractor import ScoreFeatureExtractor
-from canon_utils import euclideanDistance
+from feature_extractor import ScoreFeatureExtractor
+from canon_utils import euclideanDistance, puzzle1, puzzle2
+import pickle
 
 class CanonProblem(util.SearchProblem):
 
-	def __init__(self, scenario, costs):
+	def __init__(self, scenario):
 			self.scenario = scenario
-			self.intervalCosts = costs
-			self.reward = ScoreFeatureExtractor(bundle = music21.corpus.search('bach'))
-			# sfe.parse() 
+			# self.intervalCosts = costs
+			# self.reward = ScoreFeatureExtractor(bundle = music21.corpus.search('bach'))
+			# self.reward.parse() 
 			# Reward.chordProgressions = pickle.load(open('chords', 'r'))
 			# Reward.intervalSets = pickle.load(open('intervals', 'r'))
-			# sfe.extractNgrams([2,3,4])
+			# self.reward.extractNgrams([2,3,4])
 			# pickle.dump(sfe.chordFeatures, open('chordFeatures', 'w'))
 			# pickle.dump(sfe.intervalFeatures, open('intervalFeatures', 'w'))
+			self.reward = ScoreFeatureExtractor()
 			self.reward.features = pickle.load(open('features', 'r'))
 			# Reward.intervalFeatures = pickle.load(open('intervalFeatures', 'r'))
 
@@ -28,7 +30,12 @@ class CanonProblem(util.SearchProblem):
 	# has been made for each time slice
 	def isGoal(self, state):
 		if state == self.scenario.theme: return False
-		return self.cost(self.scenario.theme, state) < 5
+		print "COST"
+		if self.cost(self.scenario.theme, state) < .1:
+			end = self.scenario.theme
+			end.insert(0, state)
+			end.show()
+			return True
 
   # Costs are currently defined only on intervals. The weights are currently 
   # entirely arbitrary, but eventually they should be loaded from a pickle file
@@ -39,63 +46,62 @@ class CanonProblem(util.SearchProblem):
 		# 	result += self.intervalCosts[math.fabs(theme[i] - new_voice[i])]
 		# return result
 		sc = music21.stream.Score()
+		# # sc = music21.stream.Stream(cautionaryAll=True, cautionaryPitchClass=True, cautionaryNotImmediateRepeat =True)
+		# print theme.notes[0]
+		# print new_voice.notes[0]
+		print "ASD"
+		for n in new_voice.flat.getElementsByClass(music21.note.Note):
+			print n
 		sc.insert(0, theme)
 		sc.insert(0, new_voice)
-		sfe = ScoreFeatureExtractor(stream = sc)
-		sfe.parse() 
+		sfe = ScoreFeatureExtractor(streams = sc)
+		sfe.parse(music21.key.Key('c')) 
 		sfe.extractNgrams([2,3,4])
 		euc_dist = euclideanDistance(sfe.features, self.reward.features)
+		return euc_dist
 
 	def succAndCost(self, state):
 		"""returns a list of possible successor states: (action, newstate, cost)"""
 		actions = self.scenario.getActions(state)
 		succ = []
+		print actions
 		for a in actions:
+			print a[1]
 			succ.append((a[0], a[1], self.cost(self.scenario.theme, a[1])))
 		return succ
 
 
 class CanonScenario:
-	def __init__(self, description, noteMap):
-		self.noteMap = noteMap
-		self.theme = self.parseSong(description)
+	def __init__(self, theme):
+		# self.noteMap = noteMap
+		# self.theme = self.parseSong(description)
+		self.theme = theme
 
 	def getActions(self, state): #V is current vector
 		results = []
 		next_state = state
 		#Shift (Round)
 		for i in range(len(self.theme.notes)):
-			next_state = s.sliceAtOffsets(i, inPlace=True)
+			# next_state = s.sliceAtOffsets(i, inPlace=True)
 			next_state = self.theme[-1*i:] + self.theme[:-1*i] 
+			results.append(("Shift by " + str(i), next_state))
 
 		## Reversal
 		next_state = self.theme[::-1]
 		results.append(("Reverse", next_state))
 		## Transposition
 		for i in xrange(2,8):
-			next_state = self.theme.transpose(music21.interval.GenericInterval(i))
+			next_state = self.theme.transpose(music21.interval.Interval(i))
 			results.append(("Transposed by generic " + str(i), next_state))
 		return results
 
 	def __str__(self): return self.theme
 
-	def parseSong(self, song):
-		V = []
-		notes = song.split(",")
-		for n in notes:
-			pitch = n.split(":")[0]
-			duration = n.split(":")[1]
-			for i in range(int(duration)):
-				V.append(self.noteMap[pitch])
-		return tuple(V)
-
-
-
-	# def constructSong(self, state):
-	# 	for 		notes = song.split(",")
-	# 	for n in notes:
-	# 		pitch = n.split(":")[0]
-	# 		duration = n.split(":")[1]
-	# 		for i in range(int(duration)):
-	# 			V.append(self.noteMap[pitch])
-	# 	return tuple(V)
+if __name__ == "__main__":
+	print "Creating Scenario"
+	scenario = CanonScenario(puzzle1())
+	print "Defining Problem"
+	problem = CanonProblem(scenario)
+	ucs = util.UniformCostSearch(verbose=3)
+	print "Solving Canon"
+	answer = ucs.solve(problem)
--- solver/feature_extractor.py
+++ solver/feature_extractor.py
@@ -12,32 +12,61 @@ class ScoreFeatureExtractor:
 		if bundle:
 			self.streams = [x.parse() for x in bundle]
 		else:
-			self.streams = streams
+			self.streams = [streams]
 
-	def parse(self):
+	def parse(self, analyzedKey=None):
 		print "Parsing..."
 		self.chordProgressions = []
 		self.intervalSets = []
 
-		for stream in self.streams:
-			print '.'
-			try:
-				reduction = stream.chordify()
-			except music21.stream.StreamException:
-				continue
-			analyzedKey = stream.analyze('key')
-			chords = []
-			intervals = []
-			for c in reduction.flat.getElementsByClass('Chord'):
-				c.closedPosition(forceOctave=4, inPlace = True)
-				mainInterval = music21.interval.notesToChromatic(c.pitches[0], c.pitches[-1])
-				rn = music21.roman.romanNumeralFromChord(c, analyzedKey)
-				chords.append((rn.scaleDegree, rn.quality, rn.inversion()))
-				intervals.append(re.search('([0-9]+)>', str(mainInterval)).group(0)[:-1])
-			self.chordProgressions.append(chords)
-			self.intervalSets.append(intervals)
-			pickle.dump(self.chordProgressions, open('chords', 'w'))
-			pickle.dump(self.intervalSets, open('intervals', 'w'))
+
+
+		for s in self.streams:
+			cs = []
+			ins = []
+			if not analyzedKey:
+				analyzedKey = s.analyze('key')
+			if len(s.parts) < 2: continue
+			# if len(stream.parts) < 3: 
+
+				# try:
+				# 	for interval in music21.theoryAnalysis.theoryAnalyzer.getHarmonicIntervals(stream, 0, 1):
+				# 		intervals.append(re.search('([0-9]+)>', str(interval)).group(0)[:-1])
+				# except: 
+				# 	stream.parts[0].quantize()
+				# 	stream.parts[1].quantize()
+				# 	intervals = []
+
+				# 	for i in range(len(stream.parts[0])):
+				# 		notes0 = stream.parts[0].getElementsByOffset(i).notes
+				# 		notes1 = stream.parts[0].getElementsByOffset(i).notes
+				# 		if len(notes0) and len(notes1):
+				# 			n0 = notes0[0]
+				# 			n1 = notes1[0]
+				# 			intervals.append(music21.interval.notesToChromatic(n0, n1))
+				
+				# self.intervalSets.append(intervals)
+
+				# self.chordProgressions = []
+
+				# pickle.dump(self.intervalSets, open('intervals', 'w'))
+			else:
+
+				try:
+					reduction = s.chordify()
+				except music21.stream.StreamException:
+					continue
+
+				for c in reduction.flat.getElementsByClass('Chord'):
+					c.closedPosition(forceOctave=4, inPlace = True)
+					mainInterval = music21.interval.notesToChromatic(c.pitches[0], c.pitches[-1])
+					rn = music21.roman.romanNumeralFromChord(c, analyzedKey)
+					cs.append((rn.scaleDegree, rn.quality, rn.inversion()))
+					ins.append(re.search('([0-9]+)>', str(mainInterval)).group(0)[:-1])
+				self.chordProgressions.append(cs)
+				self.intervalSets.append(ins)
+				# pickle.dump(self.chordProgressions, open('chords', 'w'))
+				# pickle.dump(self.intervalSets, open('intervals', 'w'))
 
 
 	def extractNgrams(self, sizes):
@@ -48,39 +77,51 @@ class ScoreFeatureExtractor:
 
 		sizes = [x-1 for x in sizes]
 		for chordSet in self.chordProgressions:
-			for idx, chord in enumerate(chordSet):
-				key = [chord]
+			Z = float(len(chordSet))
+			for idx, c in enumerate(chordSet):
 				for n in sizes:
+					key = [c]
 					# print '..'
 					if (idx >= n): 
 						key += chordSet[idx-n:idx]
-						self.chordFeatures[tuple(key)] += 1.
-			Z = float(len(chordSet))
-			for key in self.chordFeatures.keys():
-				self.chordFeatures[key] /= Z - (len(key)-1)
-				self.features[key] = self.chordFeatures[key]
+						self.chordFeatures[tuple(key)] += 1. / (Z - n)
+		for key in self.chordFeatures.keys():
+				# self.chordFeatures[key] /= Z - (len(key)-1)
+			self.features[key] = self.chordFeatures[key]
+			# print self.features[key]
 
 		for intervalSet in self.intervalSets:
-			for idx, interval in enumerate(intervalSet):
-				key = [interval]
+			# print intervalSet
+			Z = float(len(intervalSet))
+			for idx, inter in enumerate(intervalSet):
 				for n in sizes:
+					key = [inter]
 					# print '...'
 					if (idx >= n): 
 						key += intervalSet[idx-n:idx]
-						self.intervalFeatures[tuple(key)] += 1.
-			Z = float(len(intervalSet))
-			for key in self.intervalFeatures.keys():
-				self.intervalFeatures[key] /= Z - (len(key)-1)
-				self.features[key] = self.intervalFeatures[key]
+						# print key
+						self.intervalFeatures[tuple(key)] += 1. / (Z - n)
+						# print self.intervalFeatures[tuple(key)]
+			
+			# print Z
+		for key in self.intervalFeatures.keys():
+			# 	print self.intervalFeatures[key]
+			# 	self.intervalFeatures[key] /= Z - (len(key)-1)
+			self.features[key] = self.intervalFeatures[key]
+			# print self.features[key]
 		print "...features extracted"
 
 
 if __name__ == "__main__":
 	sfe = ScoreFeatureExtractor(bundle = music21.corpus.search('bach'))
+	# sfe = ScoreFeatureExtractor()
+
 	sfe.parse() 
 	# sfe.chordProgressions = pickle.load(open('chords', 'r'))
 	# sfe.intervalSets = pickle.load(open('intervals', 'r'))
 	sfe.extractNgrams([2,3,4])
+	# print sfe.intervalFeatures
+	# print sfe.intervalFeatures
 	pickle.dump(sfe.features, open('features', 'w'))
 	# pickle.dump(sfe.intervalFeatures, open('intervalFeatures', 'w'))
 	# sfe.chordFeatures = pickle.load(open('chordFeatures', 'r'))
